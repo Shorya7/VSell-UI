bf77f8c6a636d37667ea820146738c22236748dc
Shorya <shorya2113081@akgec.ac.in>
2024-07-28 16:09:40 +0530

    initial commit--vite app

---
+++ src/assets/card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div className="container">
+      <div className="head">
+        <h1>Title</h1>
+      </div>
+      <div className="card_price">
+        <p id="price">$50</p>/month<p id="pm"></p>
+      </div>
+      <div className="items_list">
+        <p>List Item 1</p>
+        <p>List Item 1</p>
+        <p>List Item 1</p>
+        <p>List Item 1</p>
+        <p>List Item 1</p>
+        <p>List Item 1</p>
+        <p>List Item 1</p>
+      </div>
+      <div className="card_button">
+        <button className="sub">Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default card;
